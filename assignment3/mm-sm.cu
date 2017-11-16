bdeb802945279cf62fff35968f0d1de22ec3f47b
Bui Do Hiep <buidohiep@gmail.com>
2017-11-16 16:05:28 +0800

    [Assignment 3] Add shared memory version

--- assignment3/mm-sm.cu
+++ assignment3/mm-sm.cu
@@ -11,11 +11,16 @@
 #include <sys/time.h>
 #include <assert.h>
 
+#define BLOCK_SIZE 16
+
 int size;
 
 typedef struct
 {
-    float ** element;
+int width;
+int height;
+int stride;
+float ** element;
 } matrix;
 
 
@@ -43,23 +48,28 @@ void allocate_matrix(matrix* m)
     int i;
     cudaError_t rc;
     
+    m->width = size;
+    m->height = size;
+    m->stride = size;
+
     // allocate array for all the rows
     rc = cudaMallocManaged((void**)&(m->element), sizeof(float*) * size);
-    if (rc != cudaSuccess)
-    {
+    if (rc != cudaSuccess) {
         fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(rc));
         exit(1);
     }
     
+    // allocate all matrix elements in one array of continuous addresses
+    float* array;
+    rc = cudaMallocManaged((void**)&array, sizeof(float) * size * size);
+    if (rc != cudaSuccess) {
+        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(rc));
+        exit(1);
+    }
+
     // allocate an array for each row of the matrix
-    for (i = 0; i < size; i++)
-    {
-        rc = cudaMallocManaged((void**)&(m->element[i]), sizeof(float) * size);
-        if (rc != cudaSuccess)
-        {
-            fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(rc));
-            exit(1);
-        }
+    for (i = 0; i < size; i++) {
+        m->element[i] = &(array[i * size]);
     }
 }
 
@@ -67,9 +77,7 @@ void allocate_matrix(matrix* m)
 * Free the memory allocated for a matrix.
 **/
 void free_matrix(matrix* m) {
-    int i;
-    for (i = 0; i < size; i++)
-        cudaFree(m->element[i]);
+    cudaFree(m->element[0]);
     cudaFree(m->element);
 }
 
@@ -103,6 +111,27 @@ void init_matrix_zero(matrix m)
         }
 }
 
+/**
+* Get element  at row, col of sub matrix
+*/
+__device__ float get_element(const matrix a, int row, int col) {
+    return a->element[0][row * a.stride + col];
+}
+
+/**
+* Set element at row, col of sub matrix
+*/
+__device__ void set_element(const matrix a, int row, int col, float value) {
+    a->element[0][row * a.stride + col] = value;
+}
+
+__device__ matrix get_sub_matrix(matrix a, int row, int col) {
+    matrix a_sub;
+    a_sub.width = size;
+    a_sub.width = size;
+    a_sub.stride = a.stride;
+    a_sub.element = &a.element[a.stride * BLOCK_SIZE * row][BLOCK_SIZE * col];
+}
 
 /**
 * Multiplies matrix @a with matrix @b storing
@@ -127,20 +156,48 @@ void mm(matrix a, matrix b, matrix result)
 */
 __global__ void mm_kernel(matrix a, matrix b, matrix result, int size)
 {
-    int i = blockIdx.x * blockDim.x + threadIdx.x; 
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-    int k;
+    // index in the original matric
+    int g_row = blockIdx.x * blockDim.x + threadIdx.x; 
+    int g_col = blockIdx.y * blockDim.y + threadIdx.y;
+    int m, e;
 
     if (i >= size || j >= size)
         return;
 
-// Reduce number of access to global memory of result matrix
-float sum = 0;
+    // block index
+    int block_row = blockIdx.x;
+    int block_col = blockIdx.y;
 
-for(k = 0; k < size; k++)
-    sum += a.element[i][k] * b.element[k][j];
+    float result_value = 0;
+    // thread index
+    int row = threadIdx.x;
+    int col = threadIdx.y;
 
-result.element[i][j] = sum;
+    int num_block = (a.width + BLOCK_SIZE - 1) / BLOCK_SIZE);
+    for (m = 0; m < num_block; m++) {
+        matrix a_sub = get_sub_matrix(a, block_row, m);
+        matrix b_sub = get_sub_matrix(b, m, block_col);
+
+        // Shared memory to store a_sub and b_sub
+        __shared__ float a_s[BLOCK_SIZE][BLOCK_SIZE];
+        __shared__ float b_s[BLOCK_SIZE][BLOCK_SIZE];
+
+        a_s[row][col] = get_element(a_sub, row, col);
+        b_s[row][col] = get_element(a_sub, row, col);
+        // Synchronize to make sure the sub-matrices are loaded
+        // before starting the computation
+        __syncthreads();
+
+        // Multiply a_sub and b_sub together
+        for (e = 0; e < BLOCK_SIZE; ++e) {
+            result_value += a_s[row][e] * b_s[e][col];
+            // Synchronize to make sure that the preceding computation is done
+            // before loading two new sub-matrices of A and B in the next iteration
+            __syncthreads();
+        }
+    }
+
+    result[g_row][g_col] = result_value;
 }
 
 void print_matrix(matrix m)
